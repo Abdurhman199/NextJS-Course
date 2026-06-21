8f83faf09cd71c5e250c11d2c58e89a12c568787
abdurhman <abdurhman@MacBook-Pro-alkhas-b-abdurhman.local>
2026-06-21 14:50:53 +0300

    Add assignment 1

---
+++ assignment-1/Day1_Assignment1.tsx
@@ -0,0 +1,22 @@
+
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+
+      <button onClick={() => setCount(count + 1)}>
+        Add
+      </button>
+
+      <button onClick={() => setCount(0)}>
+        Reset
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
